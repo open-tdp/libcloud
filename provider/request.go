5bbd6f807a5798a621f09e3e1e129a662a989106
若海 <wang@rehiy.com>
2023-04-04 12:44:48 +0800

    feat: 增加公共配置

---
+++ provider/request.go
@@ -0,0 +1,18 @@
+package provider
+
+type ReqeustParam struct {
+	SecretId  string `note:"访问密钥 Id"`
+	SecretKey string `note:"访问密钥 Key"`
+	RegionId  string `note:"资源所在区域"`
+	Endpoint  string `note:"指定接口域名"`
+	Service   string `note:"产品名称"`
+	Version   string `note:"接口版本"`
+	Action    string `note:"接口名称"`
+	Payload   any    `note:"结构化数据"`
+}
+
+type ResponseResult struct {
+	Errors  []error  `note:"错误信息"`
+	Message []string `note:"消息内容"`
+	Payload any      `note:"请求结果"`
+}
