68cb560bf28aec4ba68f26b9cbb0af0098242554
若海 <wang@rehiy.com>
2023-10-21 19:44:21 +0800

    feat: 增加轻量基础接口

--- compute/driver/tencent_lighthouse.go
+++ compute/driver/tencent_lighthouse.go
@@ -25,42 +25,185 @@ func NewTencentLighthouseDriver(rq *provider.ReqeustParam) *TencentLighthouseDri
 
 // List all instance
 func (p *TencentLighthouseDriver) ListNodes() ([]*compute.Node, error) {
-	return nil, nil
+
+	resp, err := p.lighthouse.DescribeInstances(&lighthouse.DescribeInstancesRequest{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	var nodes []*compute.Node
+
+	for _, instance := range resp.Response.InstanceSet {
+
+		// TODO: fix mapping
+		state := compute.NodeState(*instance.InstanceState)
+		osType := compute.OSType(*instance.OsName)
+
+		node := &compute.Node{
+			Id:        *instance.InstanceId,
+			Name:      *instance.InstanceName,
+			State:     state,
+			PublicIp:  *instance.PublicAddresses[0],
+			PrivateIp: *instance.PrivateAddresses[0],
+			Size: &compute.NodeSize{
+				Id: *instance.BundleId,
+			},
+			Image: &compute.NodeImage{
+				Id:     "",
+				Name:   *instance.OsName,
+				OSType: osType,
+			},
+			Location: &compute.Location{
+				Id: *instance.Zone,
+			},
+		}
+
+		nodes = append(nodes, node)
+
+	}
+
+	return nodes, nil
+
 }
 
 // Detail instance by Id
 func (p *TencentLighthouseDriver) DetailNode(id string) (*compute.Node, error) {
-	return nil, nil
+
+	resp, err := p.lighthouse.DescribeInstances(&lighthouse.DescribeInstancesRequest{
+		InstanceIds: []*string{&id},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(resp.Response.InstanceSet) == 0 {
+		return nil, nil
+	}
+
+	instance := resp.Response.InstanceSet[0]
+
+	// TODO: fix mapping
+	state := compute.NodeState(*instance.InstanceState)
+	osType := compute.OSType(*instance.OsName)
+
+	node := &compute.Node{
+		Id:        *instance.InstanceId,
+		Name:      *instance.InstanceName,
+		State:     state,
+		PublicIp:  *instance.PublicAddresses[0],
+		PrivateIp: *instance.PrivateAddresses[0],
+		Size: &compute.NodeSize{
+			Id: *instance.BundleId,
+		},
+		Image: &compute.NodeImage{
+			Id:     "",
+			Name:   *instance.OsName,
+			OSType: osType,
+		},
+		Location: &compute.Location{
+			Id: *instance.Zone,
+		},
+	}
+
+	return node, nil
+
 }
 
 // Create new instance
 func (p *TencentLighthouseDriver) CreateNode(opts *compute.NodeCreateOpts) (*compute.Node, error) {
-	return nil, nil
+
+	resp, err := p.lighthouse.CreateInstances(&lighthouse.CreateInstancesRequest{
+		// TODO: create instance
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if len(resp.Response.InstanceIdSet) == 0 {
+		return nil, nil
+	}
+
+	instanceId := resp.Response.InstanceIdSet[0]
+
+	node, err := p.DetailNode(*instanceId)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return node, nil
+
 }
 
 // Destroy an existing instance
 func (p *TencentLighthouseDriver) DestroyNode(node *compute.Node) error {
-	return nil
+
+	_, err := p.lighthouse.TerminateInstances(&lighthouse.TerminateInstancesRequest{
+		InstanceIds: []*string{&node.Id},
+	})
+
+	return err
+
 }
 
 // Reboot instance
 func (p *TencentLighthouseDriver) RebootNode(node *compute.Node) error {
-	return nil
+
+	_, err := p.lighthouse.RebootInstances(&lighthouse.RebootInstancesRequest{
+		InstanceIds: []*string{&node.Id},
+	})
+
+	return err
+
 }
 
 // Start instance
 func (p *TencentLighthouseDriver) StartNode(node *compute.Node) error {
-	return nil
+
+	_, err := p.lighthouse.StartInstances(&lighthouse.StartInstancesRequest{
+		InstanceIds: []*string{&node.Id},
+	})
+
+	return err
+
 }
 
 // Stop instance
 func (p *TencentLighthouseDriver) StopNode(node *compute.Node) error {
-	return nil
+
+	_, err := p.lighthouse.StopInstances(&lighthouse.StopInstancesRequest{
+		InstanceIds: []*string{&node.Id},
+	})
+
+	return err
+
 }
 
 // Get the current state of instance
 func (p *TencentLighthouseDriver) GetNodeState(node *compute.Node) (compute.NodeState, error) {
-	return compute.NodeState(""), nil
+
+	resp, err := p.lighthouse.DescribeInstances(&lighthouse.DescribeInstancesRequest{
+		InstanceIds: []*string{&node.Id},
+	})
+
+	if err != nil {
+		return "", err
+	}
+
+	if len(resp.Response.InstanceSet) == 0 {
+		return "", nil
+	}
+
+	instance := resp.Response.InstanceSet[0]
+
+	// TODO: fix mapping
+	state := compute.NodeState(*instance.InstanceState)
+
+	return state, nil
+
 }
 
 // Get the Console url for instance
